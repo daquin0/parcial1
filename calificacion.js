02355520358ef2314be313ac97eed7b6b9b606c2
daquin0 <60717075+daquin0@users.noreply.github.com>
2020-02-10 16:37:44 -0600

    calificacion en letra
    conversion al sistema americano

---
+++ calificacion.js
@@ -0,0 +1,25 @@
+var x=Math.floor(Math.random()*10)+1;
+if(x==10)
+{
+    console.log(x," es A")
+}
+else if(x==9)
+{
+    console.log(x,"es B")
+}
+else if(x==8)
+{
+    console.log(x,"es C")
+}
+else if(x==7)
+{
+    console.logx(x,"es D")
+}
+else if(x==6)
+{
+    console.log(x,"es E")
+}
+else
+{
+    console.log(x,"es F")
+}
