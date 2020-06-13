21fce4733463aa78ba2c8a1eb6e86c7ef35834da
daquin0 <60717075+daquin0@users.noreply.github.com>
2020-06-13 15:59:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,36 +21,3 @@ request.onload = function() {
 request.send()
 
 });
-
-
-/*let costo = document.getElementById("txtCosto").value;
-  let color = document.getElementById("txtColor").value;
-  let peso = document.getElementById("txtPeso").value;
-  let caducidad = document.getElementById("txtCaducidad").value;
-  let cantidad = document.getElementById("txtCantidad").value;
-  let id = document.getElementById("txtId").value;
-  cantidad = parseInt(cantidad);
-  costo = parseInt(costo);
-  peso = parseInt(peso);
-  caducidad = parseInt(caducidad);
-  id = parseInt(id);
-
-  let product = new Producto(nombre, costo, color, cantidad,peso,caducidad,id);
-
-  let details = document.getElementById("details");
-  details.innerHTML += `
-    <div class="p-3 mb-2 bg-secondary text-white">
-      <p class="card-title"><strong>ID ${product.id} - </strong>${
-    product.nombre
-  }</p>
-      <p class="card-text">Desc: ${product.cantidad}</p>
-      <p class="card-text">Desc: ${product.color}</p>
-      <p class="card-text">Desc: ${product.costo}</p>
-      <p class="card-text">Desc: ${product.peso}</p>
-      <p class="card-text">Desc: ${product.caducidad}</p>
-      <p class="card-text">${app.get}</p>
-    </div>
-  `;
-  miInfo.agregar(product);
-  console.log(miInfo.listar());
-  console.log(product.showInfo());*/
